5ff9074aa3bae22063a392205c46b300970381f2
Mitchell Barclay <mitchellbarclay2@gmail.com>
2019-06-15 13:12:54 +1200

    Initial commit

---
+++ script.js
@@ -0,0 +1,146 @@
+document.addEventListener('keyup', function (event) {
+    if (event.defaultPrevented) {
+        return;
+    }
+
+    var key = event.key || event.keyCode;
+
+    if (key === 'Escape' || key === 'Esc' || key === 27) {
+        closer();
+    }
+});
+
+
+
+function feature1(){
+	var f = document.getElementById("feature");
+    var fd = document.getElementById("feature_darken");
+
+	f.style.display = "block";
+	fd.style.display = "block";
+	f.style.backgroundImage = "url('img/static/alien.png')";
+
+}
+function feature2(){
+	var f = document.getElementById("feature");
+    var fd = document.getElementById("feature_darken");
+
+	f.style.display = "block";
+	fd.style.display = "block";
+	f.style.backgroundImage = "url('img/static/concept.png')";
+}
+function feature3(){
+	var f = document.getElementById("feature");
+    var fd = document.getElementById("feature_darken");
+
+	f.style.display = "block";
+	fd.style.display = "block";
+	f.style.backgroundImage = "url('img/static/magicmorning.jpg')";
+}
+function feature4(){
+	var f = document.getElementById("feature");
+    var fd = document.getElementById("feature_darken");
+
+	f.style.display = "block";
+	fd.style.display = "block";
+	f.style.backgroundImage = "url('img/static/pres2.png')";
+}
+function feature5(){
+	var f = document.getElementById("feature");
+    var fd = document.getElementById("feature_darken");
+
+	f.style.display = "block";
+	fd.style.display = "block";
+	f.style.backgroundImage = "url('img/static/richpic.png')";
+}
+function feature6(){
+	var f = document.getElementById("feature");
+    var fd = document.getElementById("feature_darken");
+
+	f.style.display = "block";
+	fd.style.display = "block";
+	f.style.backgroundImage = "url('img/static/sink.jpg')";
+}
+function feature7(){
+	var f = document.getElementById("feature");
+    var fd = document.getElementById("feature_darken");
+
+	f.style.display = "block";
+	fd.style.display = "block";
+	f.style.backgroundImage = "url('img/static/album.png')";
+}
+
+
+
+function feature8(){
+	var f = document.getElementById("feature");
+    var fd = document.getElementById("feature_darken");
+
+	f.style.display = "block";
+	fd.style.display = "block";
+	f.style.backgroundImage = "url('img/static/pres3.png')";
+}
+function feature9(){
+	var f = document.getElementById("feature");
+    var fd = document.getElementById("feature_darken");
+
+	f.style.display = "block";
+	fd.style.display = "block";
+	f.style.backgroundImage = "url('img/static/yes.jpg')";
+}
+function feature10(){
+	var f = document.getElementById("feature");
+    var fd = document.getElementById("feature_darken");
+
+	f.style.display = "block";
+	fd.style.display = "block";
+	f.style.backgroundImage = "url('img/static/still.jpg')";
+}
+function feature11(){
+	var f = document.getElementById("feature");
+    var fd = document.getElementById("feature_darken");
+
+	f.style.display = "block";
+	fd.style.display = "block";
+	f.style.backgroundImage = "url('img/static/fiji.jpg')";
+}
+function feature12(){
+	var f = document.getElementById("feature");
+    var fd = document.getElementById("feature_darken");
+
+	f.style.display = "block";
+	fd.style.display = "block";
+	f.style.backgroundImage = "url('img/static/comic.jpeg')";
+}
+function feature13(){
+	var f = document.getElementById("feature");
+    var fd = document.getElementById("feature_darken");
+
+	f.style.display = "block";
+	fd.style.display = "block";
+	f.style.backgroundImage = "url('img/static/journey.png')";
+}
+function feature14(){
+	var f = document.getElementById("feature");
+    var fd = document.getElementById("feature_darken");
+
+	f.style.display = "block";
+	fd.style.display = "block";
+	f.style.backgroundImage = "url('img/static/3.jpg')";
+}
+
+
+
+
+function closer(){
+
+
+	var f = document.getElementById("feature");
+var fd = document.getElementById("feature_darken");
+
+	
+
+	f.style.display = "none";
+	fd.style.display = "none";
+
+}
