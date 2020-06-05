305cb2d25f910fb6edff806895b39c02a521da33
Mitchell Barclay <mitchellbarclay2@gmail.com>
2020-06-05 23:19:09 +1200

    new titles

--- script.js
+++ script.js
@@ -420,7 +420,13 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("menu").style.top = "0";
   } else {
-    document.getElementById("menu").style.top = "-75px";
+    document.getElementById("menu").style.top = "-80px";
+  }
+    if (prevScrollpos > currentScrollPos) {
+    document.getElementById("menu_buttons").style.top = "0";
+  } else {
+    document.getElementById("menu_buttons").style.top = "-80px";
   }
   prevScrollpos = currentScrollPos;
 }
+
